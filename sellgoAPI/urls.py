abf7a57d05b36926ce999621d43323ef5450435c
shubham2637 <shubham37@outlook.com>
2022-02-05 19:29:28 +0530

    testing commits

--- sellgoAPI/urls.py
+++ sellgoAPI/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('addcustomer',views.add_customer,name="Add Customer"),
     path('uploadcsv',views.uploadcsv,name="Add product csv"),
     path('getproduct',views.getproductBycustomer,name="Add product csv"),
+    #test comment git push
 ]
